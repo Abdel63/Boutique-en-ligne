270fb4f919d34bb281963298923d1f255bbfcb7e
Abdé <abdel6391@gmail.com>
2019-07-31 10:53:56 +0200

    classes produits et categorie créées

--- produits.class.php
+++ produits.class.php
@@ -1,17 +1,21 @@
 <?php
 
-class categorie
+
+class produits
 {
 
     private $_nom;
     private $_description;
+    private $_prix;
     private $_image;
 
-    function __construct($nom, $description, $image)
+
+    function __construct($nom, $description, $prix, $image)
     {
 
         $this->_nom = $nom;
         $this->_description = $description;
+        $this->_prix = $prix;
         $this->_image = $image;
     }
 }
